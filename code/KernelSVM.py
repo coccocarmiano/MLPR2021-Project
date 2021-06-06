b3d14fe659aceefa8595a585a63b098b76eb9206
coccocarmiano <s248992@studenti.polito.it>
2021-06-06 11:12:38 +0200

    Implemented Kernel RBF SVM

---
+++ code/KernelSVM.py
@@ -0,0 +1,37 @@
+import numpy as np
+import utils
+from classifiers import RBF_SVM
+
+def latex(toprint):
+    outfiletex = '../data/svm_rbf_acc.tex'
+    f = open(outfiletex, "w")
+    print(r"\begin{center}", file=f)
+    print(r"\begin{longtable}{|c|c|c|c|}", file=f)
+    print(r"\caption{Kernel SVM}\label{tab:svm_rbf_acctable}\\", file=f)
+    print(r"\hline", file=f)
+    print(r"Bias & $\gamma$ & $C$ & Error Rate \\", file=f)
+    print(r"\hline", file=f)
+    
+    for tup in toprint:
+        print(f"{tup[0]} & {tup[1]} & {tup[2]} & {(1-tup[3])*100:.3f}\\% \\\\", file=f)
+        print(r"\hline", file=f)
+
+    print(r"\hline", file=f)
+    print(r"\end{longtable}", file=f)
+    print(r"\end{center}", file=f)
+
+if __name__ == '__main__':
+    trdataset = utils.load_train_data()
+    tedataset = utils.load_test_data()
+    toprint = []
+    biases = [0, 5., 10.]
+    boundaries = [.1, .5, 1]
+    gammas = [1, 5, 10]
+    for bias in biases:
+        for gamma in gammas:
+            for boundary in boundaries:
+                scores, predictions, acc = RBF_SVM(
+                    trdataset, tedataset, gamma=gamma, reg_bias=bias, boundary=boundary)
+                toprint.append((bias, gamma, boundary, acc))
+    
+    latex(toprint)
--- code/classifiers.py
+++ code/classifiers.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy import log, pi
 from scipy.special import logsumexp
+from scipy.optimize import fmin_l_bfgs_b as minimize
 
 def SVM_lin(dataset):
     pass
@@ -23,7 +24,74 @@ def gaussian_classifier(test_dataset: np.ndarray, means, covs, prior_t: float = 
         invcov = np.linalg.inv(covs[i])
         centered = test_dataset - means[i]
         contributes = np.diag(centered.T.dot(invcov).dot(centered))
-        scores[i] += -0.5 * (cterm + cov + contributes) + priors[i]#+ log(prior_t if i == 1 else 1-prior_t)
+        scores[i] += -0.5 * (cterm + cov + contributes) + priors[i]
 
     scores = scores - logsumexp(scores, axis=0)
     return scores,  np.argmax(scores, axis=0)
+
+
+def RBF_SVM(dataset : np.ndarray, test_dataset: np.ndarray, gamma : float=1., reg_bias : float=0., boundary : float=1.) -> np.ndarray:
+    '''
+    Returns the `alpha` array and the corresponding `z_i` terms as a tuple.
+
+    `dataset` must include feature label (last one).
+
+    `gamma` is the gamma RBF parameter, use intermediate values (1.-10.)
+
+    `reg_bias` is the regularized bias term. Use small values (0.-1.)
+
+    `boundary` for each `alpha` term, constraining to `0 - boundary`. Use small values (0.-1.)
+    '''
+    # Preparing our Hij matrix
+    features, labels = dataset[:-1, :], dataset[-1, :]
+    r, c = features.shape
+    zlabels = (2*labels-1).reshape((1, c))
+    Zij = zlabels.T.dot(zlabels)
+    kernmat = np.zeros((c, c))
+    
+    # Basically we exploit broadcasting to compute `c` times a matrix
+    # Which contains the diff of sample `i` with all other
+    # Then we sum along the rows to compute the `i-th` row of matrix H
+    for idx in range(c):
+        xi = features[:, idx].reshape((r, 1))
+        diff = features - xi
+        norm = (diff * diff).sum(axis=0)
+        kernmat[idx] += norm
+    
+    kernmat *= -gamma
+    kernmat = np.exp(kernmat)
+    kernmat += reg_bias
+    Hij = Zij * kernmat
+    
+    
+    # Here do the actual minimization (maximization)
+    def to_minimize(alpha):
+        value = 0.5 * alpha.T.dot(Hij).dot(alpha) - alpha.sum()
+        gradient = Hij.dot(alpha)-1
+        return (value, gradient)
+    
+    boundaries = [(0, boundary) for elem in range(c)]
+    start = np.zeros(c)
+    alphas, _, __ = minimize(to_minimize, start, bounds=boundaries)
+    alphas = np.array(alphas > 0., dtype=float)
+
+    # Here we begin the scoring part
+    test_dataset, test_labels = test_dataset[:-1, :], test_dataset[-1, :]
+    r, c = test_dataset.shape
+    scores = np.zeros(c)
+
+    # Here we do a process very similar to the one done before
+    for idx in range(c):
+        x_t = test_dataset[:, idx].reshape((r, 1))
+        diff = features - x_t
+        norm = (diff * diff).sum(axis=0)
+        expterm = norm * -gamma
+        expterm = np.exp(expterm)
+        expterm = expterm + reg_bias
+        summatory = expterm * alphas * zlabels
+        scores[idx] = summatory.sum()
+
+    predictions = scores > 0
+    accuracy = (predictions == test_labels).sum() / len(predictions)
+
+    return scores, scores > 0, accuracy
