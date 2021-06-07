71b98f7e2e7297fa418d30bf254c5848ec95735b
coccocarmiano <s248992@studenti.polito.it>
2021-06-07 15:23:45 +0200

    Partial fix

--- code/dist.py
+++ code/dist.py
@@ -5,7 +5,7 @@ from utils import load_train_data, get_patches, green, red
 fout = '../img/dist.jpg'
 
 nfeatures = 12
-nbins = 120
+nbins = 100
 
 def visualize(data):
     selectorT = data[-1] == 1
@@ -14,25 +14,25 @@ def visualize(data):
     plt.figure(figsize=(12, 8))
     for i in range(nfeatures-1):
         plt.subplot(4, 3, i+1)
-        plt.hist(np.sort(data[i, selectorT]), density=True, stacked=True,
+        plt.hist(np.sort(data[i, selectorT]), density=True,
                  color=green, alpha=.7, bins=nbins)
-        plt.hist(np.sort(data[i, selectorF]), density=True, stacked=True,
+        plt.hist(np.sort(data[i, selectorF]), density=True,
                  color=red, alpha=.7, bins=nbins)
         plt.xticks([], [])
-        plt.yticks([], [])
+        #plt.yticks([], [])
         plt.title(f"Feature {i+1}")
     plt.subplot(4, 3, 12)
-    plt.hist(np.sort(data[-1, selectorT]), color=green)
-    plt.hist(np.sort(data[-1, selectorF]), color=red)
+    plt.hist(data[-1, selectorT], color=green)
+    plt.hist(data[-1, selectorF], color=red)
     plt.xticks([], [])
-    plt.yticks([], [])
+    #plt.yticks([], [])
     plt.title("Label")
     plt.suptitle("Feature Distribution")
     plt.figlegend(handles=get_patches(),
                   handlelength=1, loc='upper right')
     plt.tight_layout()
     plt.savefig(fout, format='jpg')
-    # plt.show()
+    #plt.show()
 
 
 def main():
