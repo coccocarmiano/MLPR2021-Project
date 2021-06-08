25d962771d35ca3d4e6830bef7a4a9705b531dc1
coccocarmiano <s248992@studenti.polito.it>
2021-06-08 14:39:50 +0200

    Remade main body

--- code/mvg.py
+++ code/mvg.py
@@ -8,16 +8,18 @@ def latex(toprint):
     f = open(outfiletex, "w")
     print(r"\caption{MVG}\label{tab:mvg_acctable}", file=f)
     print(r"\begin{center}", file=f)
-    print(r"\begin{tabular}{|c|c|c|c|}", file=f)
+    print(r"\begin{tabular}{|c|c||c|c|}", file=f)
     print(r"\hline", file=f)
-    print(r"$\pi_T$ & PCA & Error Rate & $DCF_{min}$\\", file=f)
+    print(r"\ & PCA & Error Rate & $DCF$\\", file=f)
     print(r"\hline", file=f)
     
-    toprint.sort(key=lambda x: x[3])
-    toprint = toprint[:10]
+    toprint.sort(key=lambda x: min(x[0]))
+    toprint = toprint[:3]
     for tup in toprint:
-        print(f"{tup[0]} & {tup[1]} & {(1-tup[2])*100:.3f}\\% & {tup[3]:.3f}\\\\", file=f)
+        for i in range(len(tup[0])):
+            print(f"$\\pi_T = {tup[3][i]:.2f}$ & {tup[1]} & {tup[2][i]*100:.3f} & {tup[0][i]:.3f} \\\\", file=f)
         print(r"\hline", file=f)
+
     print(r"\end{tabular}", file=f)
     print(r"\end{center}", file=f)
 
@@ -31,34 +33,38 @@ if __name__ == '__main__':
 
     _, folds = utils.kfold(trdataset)
 
-    priors = (np.arange(8) + 1 ) / 10
-    pca_it = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    priors = [.33, .50, .67]
+    npca = [11, 10, 9, 8, 7, 6, 5, 4]
 
-
-
-    for prior in priors:
-        for n in pca_it:
-            all_lab = [] 
-            for fold in folds:
-                all_labels = []
-                trs, trl = fold[0][:-1, :], fold[0][-1, :]
-                tes, tel = fold[1][:-1, :], fold[1][-1, :]
-
-                gmean, bmean = utils.fc_mean(trs[:, trl > 0]), utils.fc_mean(trs[:, trl < 1])
-                gcov, bcov = utils.fc_cov(trs[:, trl > 0]), utils.fc_cov(trs[:, trl < 1])
-
-                w, v = utils.PCA(trs, feat_label=False)
-                vt = v[:, :n]
-                proj = vt.T @ tes
-                pgmean, pbmean = vt.T @ gmean, vt.T @ bmean
-                pgcov, pbcov = vt.T @ gcov @ vt, vt.T @ bcov @ vt
-                _, predictions = gaussian_classifier(proj, [pbmean, pgmean], [pbcov, pgcov], prior_t=prior)
-                predictions = predictions.tolist()
-                all_lab += predictions
-            all_lab = np.array(all_lab)
-            nc = (all_lab == trlab).sum()
-            acc = nc/nt
-            _, dcf = utils.DCF(all_lab, trlab, prior_t=prior)
-            toprint.append((prior, n, acc, dcf))
+   
+    for nred in npca:
+        tot_scores = []
+        tot_labels = []
+        for fold in folds:
+            trs, trl = fold[0][:-1, :], fold[0][-1, :]
+            tes, tel = fold[1][:-1, :], fold[1][-1, :]
+            gmean, bmean = utils.fc_mean(trs[:, trl > 0]), utils.fc_mean(trs[:, trl < 1])
+            gcov, bcov = utils.fc_cov(trs[:, trl > 0]), utils.fc_cov(trs[:, trl < 1])
+            _, v = utils.PCA(trs, feat_label=False)
+            vt = v[:, :nred]
+            pgmean, pbmean = vt.T @ gmean, vt.T @ bmean
+            pgcov, pbcov = vt.T @ gcov @ vt, vt.T @ bcov @ vt
+            ptes = vt.T @ tes
+            scores, _ = gaussian_classifier(ptes, [pbmean, pgmean], [pbcov, pgcov])
+            tot_scores.append(scores)
+            tot_labels.append(tel)
+        scores = np.concatenate(tot_scores)
+        labels = np.concatenate(tot_labels)
+        
+        dcfs, ers = [], []
+        for prior in priors:
+            t = - np.log( prior / (1-prior))
+            pscores = scores > t
+            er = (pscores != labels).sum() / len(labels)
+            dcf, _ = utils.DCF(pscores, labels, prior_t=prior)
+            dcfs.append(dcf)
+            ers.append(er)
+        toprint.append((dcfs, nred, ers, priors))
+        
 
     latex(toprint)
