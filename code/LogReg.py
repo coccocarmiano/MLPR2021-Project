2c3ee1b31525285013d769782d024dcfb4055fbc
Ruggero Nocera <9341160+rogierz@users.noreply.github.com>
2021-06-10 12:30:17 +0200

    updated LogReg, added trained values

--- code/LogReg.py
+++ code/LogReg.py
@@ -1,5 +1,6 @@
 import numpy as np
 import utils
+import os
 import matplotlib.pyplot as plt
 import matplotlib.patches as ptc
 from classifiers import logreg, logreg_scores
@@ -26,19 +27,16 @@ def latex(toprint):
 
     f.close()
 
+def plot_PCA_lambda_minDCF(values):
+    nPCA = list(values.keys())
+    priors = values[nPCA[0]]
 
-def plot_PCA_lambda_minDCF(dataset, tag=None):
-    r, c = dataset[:-1].shape
-    lambdas = np.logspace(-9, 3, 13)
-    dcfs = np.empty(len(lambdas))
-    priors = [.1, .5, .9]
     brg = ['b', 'r', 'g']
     patches = [ptc.Patch(color=c, label=f"π = {p}") for p, c in zip(priors, brg)]
-    nPCA = [11, 9, 7, 5]
+
     fig, axs = plt.subplots(2, 2)
-    fig.legend(handles=patches, handlelength=1, loc='upper right')
-    plots = [(0, 0), (0, 1), (1, 0), (1, 1)]
-    
+    fig.legend(handles=patches, handlelength=1, loc='center right', ncol=1, borderaxespad=0.1)
+
     for ax in axs.flat:
         ax.set(xlabel='λ', ylabel='minDCF', xscale='log')
     
@@ -48,66 +46,66 @@ def plot_PCA_lambda_minDCF(dataset, tag=None):
     axs[1, 1].set_ylabel(None)
 
     for n, ax in zip(nPCA, axs.flat):
-        reduced_dataset = utils.reduce_dataset(dataset, n=n)
-        _, folds = utils.kfold(reduced_dataset, n=5)
-        ax.set_title(f", with PCA (n = {n})" if n < r else "No PCA")
+        ax.set_title(f"PCA (n = {n})" if n < nPCA[0] else "No PCA")
         for i, p in enumerate(priors):
-            for j, l in enumerate(lambdas):
-                print(f"Computing for n = {n}, p = {p}, l = {l}")
-                tot_scores = []
-                tot_label = []
-                for fold in folds:
-                    trdata = fold[0]
-                    tedata = fold[1]
-                    w, b = logreg(trdata, l)
-                    scores, _, _ = logreg_scores(tedata, w, b)
-                    tot_scores.append(scores)
-                    tot_label.append(tedata[-1])
-                tot_scores = np.concatenate(tot_scores)
-                tot_label = np.concatenate(tot_label)
-                dcfs[j] = utils.min_DCF(tot_scores, tot_label, p)
+            lambdas, dcfs = values[n][p]
             ax.plot(lambdas, dcfs, color=brg[i])
 
     fig.tight_layout()
-    plt.show(block=False)
+    plt.subplots_adjust(right=0.825)
     fig.savefig('../img/' + 'logreg_lambda_minDCF.jpg', format='jpg')
 
+
+def compute_PCA_lambda_minDCF(dataset):
+    lambdas = np.logspace(-4, 3, 8)
+    dcfs = np.empty(len(lambdas))
+    priors = [.1, .5, .9]
+    nPCA = [11, 9, 7, 5]
+    values_to_plot = {}
+
+    #Check if data has been already computed and try to load them
+    data_computed = True
+    values_to_plot = {}
+    for n in nPCA:
+        values_to_plot[n] = {}
+        for i, p in enumerate(priors):
+            if os.path.exists(f"../trained/logreg_dcfs_{n}_{i}.npy") is False:
+                data_computed = False
+                break
+            else:
+                values_to_plot[n][p] = (lambdas, np.load(f"../trained/logreg_dcfs_{n}_{i}.npy"))
+              
+
+    if data_computed is False:
+        values_to_plot = {}
+        for n in nPCA:
+            reduced_dataset = utils.reduce_dataset(dataset, n=n)
+            _, folds = utils.kfold(reduced_dataset, n=5)
+            values_to_plot[n] = {}
+            for i, p in enumerate(priors):
+                for j, l in enumerate(lambdas):
+                    print(f"Computing for n = {n}, p = {p}, l = {l}")
+                    tot_scores = []
+                    tot_label = []
+                    for fold in folds:
+                        trdata = fold[0]
+                        tedata = fold[1]
+                        w, b = logreg(trdata, l)
+                        scores, _, _ = logreg_scores(tedata, w, b)
+                        tot_scores.append(scores)
+                        tot_label.append(tedata[-1])
+                    tot_scores = np.concatenate(tot_scores)
+                    tot_label = np.concatenate(tot_label)
+                    dcfs[j] = utils.min_DCF(tot_scores, tot_label, p)
+                    with open(f"../trained/logreg_dcfs_{n}_{i}.npy", 'wb') as fname:
+                        np.save(fname, dcfs)
+            
+            values_to_plot[n][p] = (lambdas, dcfs)
+
+    return values_to_plot
 
 if __name__ == '__main__':
 
     dataset = utils.load_train_data()
-    plot_PCA_lambda_minDCF(dataset)
-
-    #def tmp():
-    #    trdataset = utils.load_train_data()
-    #    tedataset = utils.load_test_data()
-    #    _, folds = utils.kfold(trdataset, n=5)
-    #    toprint = []
-    #    lambdas = [0, 10**-9, 10**-6, 10**-3, 0.1]
-    #    priors = [.1, .50, .9]
-    #    tot_scores = []
-    #    tot_labels = []
-    #    for fold in folds:
-    #        trdata = fold[0]
-    #        tedata = fold[1]
-    #        telabel = fold[1][-1, :]
-    #        for l in lambdas:
-    #            print(f"Computing for lambda: {l}")
-    #            w, b = logreg(trdata, l)
-    #            scores, _, _ = logreg_scores(tedata, w, b)
-    #            tot_scores.append(scores)
-    #            tot_labels.append(telabel)
-    #    scores = np.concatenate(tot_scores)
-    #    labels = np.concatenate(tot_labels)
-    #    
-    #    dcfs, ers = [], []
-    #    for prior in priors:
-    #        t = - np.log( prior / (1-prior))
-    #        pscores = scores > t
-    #        er = (pscores != labels).sum() / len(labels)
-    #        dcf, _ = utils.DCF(pscores, labels, prior_t=prior)
-    #        dcfs.append(dcf)
-    #        ers.append(er)
-    #        print(er)
-    #    toprint.append((dcfs, ers, priors))
-    #    latex(toprint)
+    values_to_plot = compute_PCA_lambda_minDCF(dataset)
+    plot_PCA_lambda_minDCF(values_to_plot)
