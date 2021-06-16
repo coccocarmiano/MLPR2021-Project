2566c2cfc81cad4e7b312f086507ebc89a2e568f
coccocarmiano <s248992@studenti.polito.it>
2021-06-16 15:36:38 +0200

    Polynomial SVMs code

--- code/PolySVM_Normalized_Whitened.py
+++ code/PolySVM_Normalized_Whitened.py
@@ -2,19 +2,15 @@ import utils
 import classifiers
 import numpy as np
 
-filename = 'KernelSVM_Normalized_Whiten.txt'
+filename = 'PolySVM_Normalized_Whitened.txt'
 
-def get_kernel_function(gamma, regbias):
-    def kernel_function(x1, x2):
-        diff = x1 - x2
-        diff = diff * diff
-        diff = diff.sum()
-        diff = diff * gamma
-        diff = diff * -1
-        value = np.exp(diff)
-        value = value + regbias
+def get_poly_function(c, d):
+    def poly_function(x1, x2):
+        value = x1.T @ x2
+        value = value + c
+        value = value ** d
         return value
-    return kernel_function
+    return poly_function
 
 if __name__ == '__main__':
     dataset = utils.load_train_data()
@@ -22,29 +18,29 @@ if __name__ == '__main__':
     dataset = utils.whiten(dataset)
     _, folds = utils.kfold(dataset, n=10)
     outfile = open(filename, 'w')
-    regbiases = [.01, .05, .1, .15, .25, .5]
-    gammas = [.1, .15, .3, .5, .7, 1., 1.5, 3, 5, 10]
+    constants = [0, .5, 1, 3, 5, 10]
+    powers = [1, 2, 3, 4, 5]
     bounds = [.1, .3, .5, .7, 1]
 
-    for gamma in gammas:
-        for regbias in regbiases:
+    for power in powers:
+        for constant in constants:
             for bound in bounds:
-                kernel_function = get_kernel_function(gamma, regbias)
+                poly_function = get_poly_function(power, constant)
                 scores, labels = [], []
                 for fold in folds:
                     train, test = fold[0], fold[1]
                     fold_labels = test[-1, :]
                     labels.append(fold_labels)
 
-                    alphas = classifiers.DualSVM_Train(train, kernel_function, bound=bound)
+                    alphas = classifiers.DualSVM_Train(train, poly_function, bound=bound)
                     train, alphas = utils.support_vectors(train, alphas)
-                    fold_scores = classifiers.DualSVM_Score(train, kernel_function, alphas, test)
+                    fold_scores = classifiers.DualSVM_Score(train, poly_function, alphas, test)
                     scores.append(fold_scores)
                 
                 scores = np.concatenate(scores)
                 labels = np.concatenate(labels)
                 mindcf, optimal_threshold = utils.minDCF(scores, labels, prior_t=.5)
                 # Ignore the first field, is just handy for sorting
-                print(f"{mindcf} |.| MinDCF: {mindcf:.4f}  -  Opt. Thr.: {optimal_threshold:.4f}  -  Gamma: {gamma:.2f}  -  Reg. Bias: {regbias:.2f}  -  C:   {bound:.2f}", file=outfile)
+                print(f"{mindcf} |.| MinDCF: {mindcf:.4f}  -  Opt. Thr.: {optimal_threshold:.4f}  -  Power: {power:.2f}  -  Reg. Bias: {constant:.2f}  -  C:   {bound:.2f}", file=outfile)
 
     outfile.close()
